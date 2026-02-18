78e94f880d41049af55dd83cfd3b663559f6ef76
gasparyanam <gasparyanam8@gmail.com>
2026-02-18 21:19:35 +0400

    hex

--- ft_printf_hex.c
+++ ft_printf_hex.c
@@ -6,38 +6,16 @@
 /*   By: agaspary <agaspary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 02:03:06 by agaspary          #+#    #+#             */
-/*   Updated: 2026/02/18 19:36:55 by agaspary         ###   ########.fr       */
+/*   Updated: 2026/02/18 21:13:11 by agaspary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/* int	ft_puthex_bc(unsigned long n, int upper)
+int	ft_puthex_bc(unsigned long dec, int x)
 {
 	int		count;
 	char	*base;
-
-	if (upper)
-		base = "0123456789ABCDEF";
-	else
-		base = "0123456789abcdef";
-	if (ft_strlen(base) != 16)
-		return (0);
-	count = 0;
-	if (n >= 16)
-	{
-		count += ft_puthex_bc(n / 16, upper);
-		count += ft_putchar_bc(base[n % 16]);
-	}
-	else
-		count += ft_putchar_bc(base[n]);
-	return (count);
-} */
-
-int ft_puthex_bc(unsigned long dec, int x)
-{
-	int count;
-	char *base;
 
 	if (x == 'X')
 		base = "0123456789ABCDEF";
@@ -45,8 +23,12 @@ int ft_puthex_bc(unsigned long dec, int x)
 		base = "0123456789abcdef";
 	count = 0;
 	if (dec >= 16)
+	{
 		count += ft_puthex_bc(dec / 16, x);
-	count += ft_putchar_bc(base[dec % 16]);
+		count += ft_putchar_bc(base[dec % 16]);
+	}
+	else
+		count += ft_putchar_bc(base[dec]);
 	return (count);
 }
 
